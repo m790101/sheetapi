3f56668d27e6128f8ab6e25ac5bdb14157c58b9f
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2024-03-04 17:59:02 +0800

    ad mysql2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { google } = require("googleapis");
-
+var mysql = require('mysql2');
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.json())
@@ -18,94 +18,112 @@ const allowCrossDomain = (req, res, next) => {
 
 app.use(allowCrossDomain)
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '00000000',
+  database : 'test'
+});
 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
 
 app.get("/", async(req, res) => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-      });
+  connection.query('SELECT * FROM items ', function (error, results, fields) {
+    if (error) throw error;
+    console.log('result ', results);
+  });
+
       res.send('yoooo')
 
 })
 
-app.post("/", async (req, res) => {
-    const { request, name } = req.body;
+
+
+
+// app.post("/", async (req, res) => {
+//     const { request, name } = req.body;
   
-    const auth = new google.auth.GoogleAuth({
-      keyFile: "credentials.json",
-      scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
+//     const auth = new google.auth.GoogleAuth({
+//       keyFile: "credentials.json",
+//       scopes: "https://www.googleapis.com/auth/spreadsheets",
+//     });
   
-    // Create client instance for auth
-    const client = await auth.getClient();
+//     // Create client instance for auth
+//     const client = await auth.getClient();
   
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: "v4", auth: client });
+//     // Instance of Google Sheets API
+//     const googleSheets = google.sheets({ version: "v4", auth: client });
   
-    const spreadsheetId = "1JOI3bsoUtouqjy83TKuCyYPOo9TuF5_bPV_kevu0P_I";
+//     const spreadsheetId = "1JOI3bsoUtouqjy83TKuCyYPOo9TuF5_bPV_kevu0P_I";
   
-    // Get metadata about spreadsheet
-    const metaData = await googleSheets.spreadsheets.get({
-      auth,
-      spreadsheetId,
-    });
+//     // Get metadata about spreadsheet
+//     const metaData = await googleSheets.spreadsheets.get({
+//       auth,
+//       spreadsheetId,
+//     });
   
-    // Read rows from spreadsheet
-    const getRows = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: "Sheet1!A:A",
-    });
+//     // Read rows from spreadsheet
+//     const getRows = await googleSheets.spreadsheets.values.get({
+//       auth,
+//       spreadsheetId,
+//       range: "Sheet1!A:A",
+//     });
   
-    // Write row(s) to spreadsheet
-    await googleSheets.spreadsheets.values.append({
-      auth,
-      spreadsheetId,
-      range: "Sheet1!A:B",
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [[request, name]],
-      },
-    });
+//     // Write row(s) to spreadsheet
+//     await googleSheets.spreadsheets.values.append({
+//       auth,
+//       spreadsheetId,
+//       range: "Sheet1!A:B",
+//       valueInputOption: "USER_ENTERED",
+//       resource: {
+//         values: [[request, name]],
+//       },
+//     });
   
-    res.send("Successfully submitted! Thank you!");
-  });
+//     res.send("Successfully submitted! Thank you!");
+//   });
 
 
 
 
 
-  app.post("/add/row", async (req, res) => {
-    const { id } = req.body;
+//   app.post("/add/row", async (req, res) => {
+//     const { id, number } = req.body;
 
-    console.log(req.body,id)
-    const auth = new google.auth.GoogleAuth({
-      keyFile: "credentials.json",
-      scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
+//     console.log(req.body,id)
+//     const auth = new google.auth.GoogleAuth({
+//       keyFile: "credentials.json",
+//       scopes: "https://www.googleapis.com/auth/spreadsheets",
+//     });
   
-    // Create client instance for auth
-    const client = await auth.getClient();
+//     // Create client instance for auth
+//     const client = await auth.getClient();
   
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: "v4", auth: client });
+//     // Instance of Google Sheets API
+//     const googleSheets = google.sheets({ version: "v4", auth: client });
   
-    const spreadsheetId = "1JOI3bsoUtouqjy83TKuCyYPOo9TuF5_bPV_kevu0P_I";
+//     const spreadsheetId = "1JOI3bsoUtouqjy83TKuCyYPOo9TuF5_bPV_kevu0P_I";
   
-    // Write row(s) to spreadsheet
-    await googleSheets.spreadsheets.values.update({
-      auth,
-      spreadsheetId,
-      range: `Sheet1!G${id}`,
-      valueInputOption: "RAW",
-      resource: {
-        values: [[123]],
-      },
-    });
+//     // Write row(s) to spreadsheet
+//     await googleSheets.spreadsheets.values.update({
+//       auth,
+//       spreadsheetId,
+//       range: `Sheet1!G${id}`,
+//       valueInputOption: "RAW",
+//       resource: {
+//         values: [[123]],
+//       },
+//     });
   
-    res.send("Successfully submitted! Thank you!");
-  });
+//     res.send("Successfully submitted! Thank you!");
+//   });
 
 
 app.listen(1337, (req, res) => console.log("running on 1337"));
